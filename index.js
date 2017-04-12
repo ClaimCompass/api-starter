37e18b60764bfb420fb6e554da297ba3fe8cb41f
Velizar Shulev <velizar.shulev@gmail.com>
2017-04-13 01:26:20 +0300

    Add nodemon

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-require('./lib')
+'use strict'
+
+const env = require('config').get('environment')
+
+if (env === 'development') {
+  const nodemon = require('nodemon')
+  nodemon('lib')
+  nodemon.on('restart', () => console.log('app restarting...'))
+} else {
+  require('./lib')
+}
